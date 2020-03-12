2613fd08e642c14aa23d97eb25ee152c04ae0ce1
Stanisław Bański <staban95@gmail.com>
2020-03-12 21:33:57 +0100

    Missing mappings and config for cloud front url

--- WebAdvert.Web/Helpers/WebSiteProfile.cs
+++ WebAdvert.Web/Helpers/WebSiteProfile.cs
@@ -1,5 +1,9 @@
-﻿using AutoMapper;
+﻿using AdvertApi.Models;
+using AutoMapper;
+using WebAdvert.Web.Models;
 using WebAdvert.Web.Models.AdvertManagement;
+using WebAdvert.Web.Models.Home;
+using WebAdvert.Web.ServiceClients;
 using WebAdvert.Web.ServiceClients.AdvertApi;
 
 namespace WebAdvert.Web.Helpers
@@ -9,6 +13,24 @@ namespace WebAdvert.Web.Helpers
         public WebSiteProfile()
         {
             CreateMap<CreateAdvertViewModel, CreateAdvertModel>().ReverseMap();
+
+            CreateMap<AdvertModel, Advertisement>().ReverseMap();
+
+            CreateMap<Advertisement, IndexViewModel>()
+                .ForMember(
+                    dest => dest.Title, src => src.MapFrom(f => f.Title)
+                )
+                .ForMember(
+                    dest => dest.Image, src => src.MapFrom(f => f.FilePath)
+                );
+
+            CreateMap<AdvertType, SearchViewModel>()
+                .ForMember(
+                    dest => dest.Id, src => src.MapFrom(f => f.Id)
+                )
+                .ForMember(
+                    dest => dest.Title, src => src.MapFrom(f => f.Title)
+                );
         }
     }
 }
