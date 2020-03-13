6decb2648a9ccf317ede0b23b9ddf1ab48dee06a
Stanisław Bański <staban95@gmail.com>
2020-03-13 17:28:21 +0100

    Internal api example + fixes

---
+++ WebAdvert.Web/Controllers/InternalApis.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebAdvert.Web.ServiceClients;
+
+namespace WebAdvert.Web.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    [Produces("application/json")]
+    public class InternalApis : Controller
+    {
+        private readonly IAdvertApiClient advertApiClient;
+
+        public InternalApis(IAdvertApiClient advertApiClient)
+        {
+            this.advertApiClient = advertApiClient;
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<IActionResult> GetAsync(string id)
+        {
+            var record = await advertApiClient.GetAsync(id).ConfigureAwait(false);
+            return Json(record);
+        }
+    }
+}
